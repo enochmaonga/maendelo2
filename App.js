6d422f6410279a9608894cd4cac44940d52c71b4
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-05-08 18:50:27 +0300

    initial files setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from react;
+class app extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Repair Booking Form</h1>
+            </div>
+    
+        );
+    }
+    
+}
+export default app;
